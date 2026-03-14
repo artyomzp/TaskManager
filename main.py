7f4e6e2e2f2d794c1d175b56425ce71bccdf7c4a
Artem <serdyukartyom.zp@gmail.com>
2026-03-14 12:42:18 +0200

    list comprehensive

--- main.py
+++ main.py
@@ -1,15 +1,35 @@
-todos = []
-
 while True:
     user_action = input("Type add, show, edit or exit: ").strip()
     
     match user_action:
         case "add":
-            todo = input("Enter todo: ")
+            todo = input("Enter todo: ") + "\n"
+
+            file = open('files/todos.txt','r')
+            todos = file.readlines()
+            file.close()
+
             todos.append(todo.title())
+
+            file = open('files/todos.txt','w')
+            file.writelines(todos)
+            file.close()
+
         case "show":
-            print(todos)
+            file = open('files/todos.txt','r')
+            todos = file.readlines()
+            file.close()
+
+            cleaned_todos = [item.strip("\n") for item in todos]
+
+            for index, item in enumerate(cleaned_todos):
+                string = f"{index + 1}-{item}"
+                print(string)
+
         case "edit":
+            file = open('files/todos.txt','r')
+            todos = file.readlines()
+            file.close()
             number = input("Number todo to edit: ")
             if(number > len(todos)):
                 print("There is no such todo in the list")
@@ -21,6 +41,9 @@ while True:
                 print("list is empty")
                 break
             todos[index] = input("Enter new value: ")
+        case "complete":
+            number = input("Number todo to complete: ")
+            todos.pop(number -1)
         case "exit":
             break
         case _:

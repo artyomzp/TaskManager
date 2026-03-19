483b4772444a8f2803f4347dcfd80e0f04b2e730
Artem <serdyukartyom.zp@gmail.com>
2026-03-19 14:58:57 +0200

    add edit functionality

--- gui.py
+++ gui.py
@@ -4,18 +4,30 @@ import FreeSimpleGUI as sg
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter to do", key="todo")
 add_button = sg.Button("Add")
+list_box = sg.Listbox(values=functions.get_todos(), key="todos", enable_events=True, size=[45 ,10])
+edit_button = sg.Button("Edit")
+
 
 window = sg.Window("Todo list", 
-                layout=[[label], [input_box, add_button]],
+                layout=[[label], [input_box, add_button], [list_box, edit_button]],
                 font=("Helvetica", 20))
 while True:
     event, values = window.read()
     print(event)
     print(values)
     match event:
-        case "add" :
+        case "Add":
             todos = functions.get_todos()
             todos.append(values['todo'] + "\n")
+            functions.write_todos(todos)
+            window["todos"].update(value=todos)
+        case "Edit":
+            todo_to_edit = values["todos"][0]
+            new_value = values["todo"]
+            functions.write_todos(todos)
+            window["todos"].update(value=todos)
+        case "todos":
+            window["todo"].update(value=values["todos"][0])
         case sg.WIN_CLOSED:
             break
 

b14ef55faada8b6eb81f5c79f3687a128718c4fc
Artem <serdyukartyom.zp@gmail.com>
2026-03-20 14:49:59 +0200

    add base web gui

---
+++ web.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import functions
+
+todos = functions.get_todos()
+
+def add_todo():
+    new_todo = st.session_state["new_todo"] + "\n"
+    todos.append(new_todo)
+    functions.write_todos(todos)
+
+st.title("Todos")
+st.write("This app is to increase your productivity")
+
+for todo in todos:
+    st.checkbox(todo)
+
+st.text_input(label="", placeholder="Eneter todo", 
+              on_change=add_todo, key="new_todo")

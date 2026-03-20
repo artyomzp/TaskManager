546a591a314a30e9fcf6170dba7f0651529b63bf
Artem <serdyukartyom.zp@gmail.com>
2026-03-20 15:32:42 +0200

    add complete functionality

--- web.py
+++ web.py
@@ -11,8 +11,13 @@ def add_todo():
 st.title("Todos")
 st.write("This app is to increase your productivity")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox :
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
 
 st.text_input(label="", placeholder="Eneter todo", 
               on_change=add_todo, key="new_todo")
